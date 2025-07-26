6706a3d9c825440906ad942b4881b5528512b681
BigtreeUniversal <bigtreeconnectioncom@gmail.com>
2025-07-26 18:42:18 +0200

    Add files via upload
    Signed-off-by: BigtreeUniversal <bigtreeconnectioncom@gmail.com>

---
+++ syncrolink-server.js
@@ -0,0 +1,11 @@
+{
+  "name": "synchrolink-server",
+  "version": "1.0.0",
+  "main": "synchrolink-server.js",
+  "scripts": {
+    "start": "node synchrolink-server.js"
+  },
+  "dependencies": {
+    "ws": "^8.15.0"
+  }
+}
