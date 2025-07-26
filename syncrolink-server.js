fde11d6369affa5dbbda49ad8f5a685e279a06f8
BigtreeUniversal <bigtreeconnectioncom@gmail.com>
2025-07-26 21:07:32 +0200

    Update syncrolink-server.js
    Signed-off-by: BigtreeUniversal <bigtreeconnectioncom@gmail.com>

--- syncrolink-server.js
+++ syncrolink-server.js
@@ -1,14 +1,14 @@
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Autorise toutes les origines (utile pour les tests)
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Le serveur répond bien ! ✅');
+  res.send("✅ Le serveur répond !");
 });
 
 app.listen(PORT, () => {
