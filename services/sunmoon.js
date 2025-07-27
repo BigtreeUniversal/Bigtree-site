b1cd0691a51e7cfcb1f29cd64ec2610329ffe92a
BigtreeUniversal <bigtreeconnectioncom@gmail.com>
2025-07-27 12:48:33 +0200

    Update sunmoon.js
    Signed-off-by: BigtreeUniversal <bigtreeconnectioncom@gmail.com>

--- services/sunmoon.js
+++ services/sunmoon.js
@@ -1,17 +1,10 @@
-
-const express = require('express');
 const SunCalc = require('suncalc');
-const router = express.Router();
 
-router.get('/', (req, res) => {
-  const lat = parseFloat(req.query.lat) || 0;
-  const lon = parseFloat(req.query.lon) || 0;
-  const now = new Date();
+function getSunMoonPosition(lat = 0, lon = 0, date = new Date()) {
+  const sun = SunCalc.getPosition(date, lat, lon);
+  const moon = SunCalc.getMoonPosition(date, lat, lon);
 
-  const sun = SunCalc.getPosition(now, lat, lon);
-  const moon = SunCalc.getMoonPosition(now, lat, lon);
-
-  res.json({
+  return {
     sun: {
       azimuth: (sun.azimuth * 180 / Math.PI + 180) % 360,
       altitude: sun.altitude * 180 / Math.PI
@@ -20,7 +13,7 @@ router.get('/', (req, res) => {
       azimuth: (moon.azimuth * 180 / Math.PI + 180) % 360,
       altitude: moon.altitude * 180 / Math.PI
     }
-  });
-});
+  };
+}
 
-module.exports = router;
+module.exports = { getSunMoonPosition };
