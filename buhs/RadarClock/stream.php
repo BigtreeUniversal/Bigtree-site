87f3ccd367786a913cb7a38d86a0342571b1a5ed
BigtreeUniversal <bigtreeconnectioncom@gmail.com>
2026-03-16 17:46:47 +0100

    Update stream.php
    Signed-off-by: BigtreeUniversal <bigtreeconnectioncom@gmail.com>

--- buhs/RadarClock/stream.php
+++ buhs/RadarClock/stream.php
@@ -1,15 +1,21 @@
 <?php
-// stream.php
-// Ce script sert de relais pour transformer le flux HTTP en HTTPS
-header('Content-Type: audio/mpeg');
-$url = "http://83.228.222.219:8000/stream";
-$handle = fopen($url, "rb");
+// Désactiver la limite de temps pour que le flux ne coupe pas
+set_time_limit(0);
 
-if ($handle) {
-    while (!feof($handle)) {
-        echo fread($handle, 8192);
-        flush();
-    }
-    fclose($handle);
-}
+$url = "http://83.228.222.219:8000/stream";
+
+header('Content-Type: audio/mpeg');
+header('Cache-Control: no-cache');
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, false); // Envoie directement au navigateur
+curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $data) {
+    echo $data;
+    return strlen($data);
+});
+
+curl_exec($ch);
+curl_close($ch);
 ?>
